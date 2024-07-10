a4882696672cc8c4f156c73cca2ca027dc25f074
HaTaeMin <gkxoals33@gmail.com>
2024-07-10 19:46:23 +0900

    초기 세팅

---
+++ src/main/java/com/project/mhnbackend/MhnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.mhnbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MhnBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MhnBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/mhnbackend/MhnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.mhnbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
