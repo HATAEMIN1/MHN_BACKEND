3bfd1f6c24d9eba10c8b4c46bd349ee120260ab9
chloeyeo <yeochloe01@gmail.com>
2024-07-22 17:49:09 +0900

    update - merge with chat

--- src/main/java/com/project/mhnbackend/chatBoard/dto/ChatRoomDTO.java
+++ src/main/java/com/project/mhnbackend/chatBoard/dto/ChatRoomDTO.java
@@ -1,17 +1,17 @@
-//package com.project.mhnbackend.chatBoard.dto;
-//
-//import com.project.mhnbackend.chatBoard.domain.ChatMessage;
-//import com.project.mhnbackend.chatBoard.domain.ChatRoom;
-//import lombok.AllArgsConstructor;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-//
-//import java.util.List;
-//
-//@Data
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class ChatRoomDTO {
-//    private ChatRoom chatRoom;
-//    private List<ChatMessage> messages;
-//}
+package com.project.mhnbackend.chatBoard.dto;
+
+import com.project.mhnbackend.chatBoard.domain.ChatRoom;
+import com.project.mhnbackend.chatBoard.mongo.domain.ChatMessage;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChatRoomDTO {
+    private ChatRoom chatRoom;
+    private List<ChatMessage> messages;
+}
