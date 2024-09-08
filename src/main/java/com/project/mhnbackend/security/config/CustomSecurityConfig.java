6cb736ca2fb158cefa5888c793667216428f5df0
HaTaeMin <gkxoals33@gmail.com>
2024-09-08 14:44:16 +0900

    2024-09-08 2:44배포

--- src/main/java/com/project/mhnbackend/security/config/CustomSecurityConfig.java
+++ src/main/java/com/project/mhnbackend/security/config/CustomSecurityConfig.java
@@ -73,7 +73,7 @@ public class CustomSecurityConfig {
    @Bean
    public CorsConfigurationSource corsConfigurationSource() {
       CorsConfiguration configuration = new CorsConfiguration();
-      configuration.setAllowedOriginPatterns(List.of("http://localhost:3000")); // 프론트엔드 주소 허용
+      configuration.setAllowedOriginPatterns(List.of("http://localhost:3000","https://d1wfzg8czg7veg.cloudfront.net/")); // 프론트엔드 주소 허용
       configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")); // 모든 HTTP 메소드 허용
       configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type")); // 모든 헤더 허용
       configuration.setAllowCredentials(true); // 자격 증명 허용
