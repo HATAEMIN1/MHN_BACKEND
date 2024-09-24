0e4c7cb5440fe5d9fb1fdeac691def13c153a702
HaTaeMin <gkxoals33@gmail.com>
2024-09-24 19:36:19 +0900

    페이먼트dto수정

--- src/main/java/com/project/mhnbackend/payment/dto/response/PaymentResponseDTO.java
+++ src/main/java/com/project/mhnbackend/payment/dto/response/PaymentResponseDTO.java
@@ -1,5 +1,6 @@
 package com.project.mhnbackend.payment.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 
 import java.util.List;
@@ -11,10 +12,10 @@ public class PaymentResponseDTO {
     private List<PaymentData> response;  // 변경된 부분
 
     @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class PaymentData {
         private String imp_uid;
         private String merchant_uid;
         private int amount;
         private String customer_uid;
     }
-}
